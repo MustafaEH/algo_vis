1b072f9ccd61c773f35f4f35b5dc3091861f3096
Mustafa <mustafaehab060@gmail.com>
2025-07-17 17:47:01 +0300

    initialize the project

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:algorithm_visualizer/visualizer_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const VisualizerApp());
+}
---
+++ lib/visualizer_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VisualizerApp extends StatelessWidget {
+  const VisualizerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+
+          body: Center(child: Text('Hello, World!'))),
+
+    );
+  }
+}
